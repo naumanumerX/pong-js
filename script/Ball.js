a8daf051f58b16b2ae74426f130d215b165f9927
Nauman <naumanumer10@outlook.com>
2023-10-06 20:10:26 +0100

    Adding Screen Boundaries: Ball doesnot go out of screen

--- script/Ball.js
+++ script/Ball.js
@@ -1,19 +1,60 @@
-
-export default  class Ball{
-
-    constructor(ballELe){
-        this.ballEle=ballELe;
+const INITIAL_VELOCITY=0.025;
+const VELOCITY_INCREASE=0.00001;
+export default class Ball {
+    constructor(ballELe) {
+        this.ballEle = ballELe;
+        this.reset();
     }
 
-    get x(){
-        return  parseFloat(getComputedStyle(this.ballEle).getPropertyValue("--x"))
-
+    get x() {
+        return parseFloat(getComputedStyle(this.ballEle).getPropertyValue("--x"));
     }
-    set x(value){
-        this.ballEle.style.setProperty("--x",value)
-    }
-    update(delta){
-        this.x=5;
 
+    set x(value) {
+        this.ballEle.style.setProperty("--x", value);
+    }
+
+    get y() {
+        return parseFloat(getComputedStyle(this.ballEle).getPropertyValue("--y"));
+    }
+
+    set y(value) {
+        this.ballEle.style.setProperty("--y", value);
+    }
+
+    rect(){
+        return this.ballEle.getBoundingClientRect()
+    }
+    reset() {
+        this.x = 50;
+        this.y = 50;
+        this.direction = { x: 0, y: 0 }; 
+        while (Math.abs(this.direction.x) <= 0.2 || Math.abs(this.direction.x) >= 0.9) {
+            const heading = this.randomNumberBetween(0, 2 * Math.PI);
+            this.direction = { x: Math.cos(heading), y: Math.sin(heading) };
+        }
+        this.velocity=INITIAL_VELOCITY
+    }
+
+    update(delta) {
+        // Update the ball's position based on delta time
+        this.x +=this.direction.x*this.velocity*delta;
+        this.y +=this.direction.y*this.velocity*delta;
+        this.velocity +=VELOCITY_INCREASE*delta;
+        const rect=this.rect();
+        if(rect.bottom>=window.innerHeight || rect.top<=0)
+            {
+                this.direction.y*=-1;
+            }   
+            if(rect.right>=window.innerWidth || rect.left<=0)
+            {
+                this.direction.x*=-1;
+            }  
+} 
+
+   
+
+    randomNumberBetween(min, max) {
+        return Math.random() * (max - min) + min;
     }
 }
