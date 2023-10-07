04cb935cee611c9d8b54e133c4b03cf75158e308
Nauman <naumanumer10@outlook.com>
2023-10-07 19:51:01 +0100

    Add AI opponent: Implement computer-controlled opponent for single-player mode.

---
+++ script/Paddle.js
@@ -0,0 +1,29 @@
+const SPEED = 0.02;
+
+export default class Paddle {
+  constructor(paddleEle) {
+    this.paddleEle = paddleEle;
+    this.paddleRects = paddleRects;
+    this.reset();
+  }
+
+  get position() {
+    return parseFloat(getComputedStyle(this.paddleEle).getPropertyValue("--position"));
+  }
+
+  set position(value) {
+    this.paddleEle.style.setProperty("--position", value + "%");
+  }
+
+  rect() {
+    return this.paddleEle.getBoundingClientRect();
+  }
+
+  reset() {
+    this.position = 50;
+  }
+
+  update(delta, ballHeight) {
+    this.position += SPEED * delta * (ballHeight - this.position);
+  }
+}
